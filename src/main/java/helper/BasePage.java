0b1ad96080652afc4cf79686d34b3a38cc78cf5d
annarpetrosyan@gmail.com <annarpetrosyan@gmail.com>
2017-12-18 20:30:38 +0400

    Changes in Base Page

--- src/main/java/helper/BasePage.java
+++ src/main/java/helper/BasePage.java
@@ -20,14 +20,53 @@ public class BasePage {
     }
 
     public void fill(By locator, String value){
-        find(locator).sendKeys(value);
+        fill(find(locator),value);
+//        find(locator).sendKeys(value);
+    }
+
+    public void fill(WebElement element, String value){
+        element.sendKeys(value);
+    }
+
+    public void fill(String cssSelector, String text){
+        fill(find(cssSelector), text);
     }
 
     public void click(By locator){
-        find(locator).click();
+//        find(locator).click();
+        click(find(locator));
     }
 
-    public boolean isDisplayed(By locator){
-        return find(locator).isDisplayed();
+    public void click(WebElement element){
+        element.click();
     }
+
+    public WebElement find(String cssSelector){
+        return find(By.cssSelector(cssSelector));
+    }
+
+    public void click(String cssSelector){
+        click(find(cssSelector));
+//        click(find(By.cssSelector(cssSelector)));
+    }
+
+    public boolean isDisplayed(String cssSelector){
+        return isDisplayed(find(cssSelector));
+//        return find(locator).isDisplayed();
+    }
+
+
+    public boolean isDisplayed(By locator){
+        return isDisplayed(find(locator));
+//        return find(locator).isDisplayed();
+    }
+
+    public boolean isDisplayed(WebElement element) {
+       try {
+            return element.isDisplayed();
+        } catch (NoSuchFieldError e) {
+            return false;
+        }
+    }
+
 }
